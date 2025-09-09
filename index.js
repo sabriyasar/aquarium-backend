b56aa036e3597815dbf66dd94038dfadd00b281e
sabriyasar <info@sabriyasar.com.tr>
2025-09-09 22:29:24 +0300

    fixed bug cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:4040",                 // local frontend
-      "aquarium-web-app.vercel.app"    // deployed frontend
+      "https://aquarium-web-app.vercel.app/"    // deployed frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
